59cafee8d3c000a094249ac90d04eb808fe5e177
OG0808 <ogarns@proton.me>
2023-11-29 18:17:33 -0500

    conflicto de sintaxis en el componente home aplicar metodologia BEM

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Nabvar from "./components/Navbar"
 import Home from "./pages/Home"
-import Carrousel from "./components/Carousel"
+
 import "./App.css";
 
 
