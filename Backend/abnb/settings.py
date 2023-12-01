aa22181968db4e4eabe704c234a5290e55ddd3b4
Carlos Silva <carlossilva@ufam.edu.br>
2023-11-30 20:13:52 -0400

    defining applications

--- Backend/abnb/settings.py
+++ Backend/abnb/settings.py
@@ -30,6 +30,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'usuario',
+    'propriedad',
+    'reserva',
+    'comentarios'
+
 ]
 
 MIDDLEWARE = [
