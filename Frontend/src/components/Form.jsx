116e55f8a790fcb99ae5d72ed9c09c190e63c582
OG0808 <ogarns@proton.me>
2023-12-03 17:00:23 -0500

    componente form ubicado en la carpeta components

--- Frontend/src/components/Form.jsx
+++ Frontend/src/components/Form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
-import axiosClient from './../../config/axiosClient';
-import Alert from "../../components/Alert";
+import axiosClient from '../config/axiosClient';
+import Alert from "./Alert";
 
 const Form = () => {
   const [firstName, setFirstName] = useState('')
