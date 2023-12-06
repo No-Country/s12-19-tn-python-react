d7cb903c558f9602f77662be1885ac7d4d124089
Alien3k <estebanquereilhac@gmail.com>
2023-12-06 12:00:35 -0300

    Se agregó formulario de propiedad, servicios con check enrutamiento y un css basico para el registro de usuario y se solucionaron problemas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,22 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Form from "./Pages/Register/Form";
 import Inicio from "./pages/Inicio";
-import { useState } from "react";
-import AuthLayout from "./layouts/AuthLayout";
 import Navbar from "./components/Navbar";
 import NewPorpertie from "./Pages/NewPropertie";
 
 
 
 function App() {
-  const [showNavbar, setShowNavbar] = useState(true)
-
   return (
     <>      
     <Navbar />
       <Routes>
-        <Route path="/" element={<AuthLayout />} />
-        <Route index element={<Inicio/>} />
+        <Route path="/" element={<Inicio />} />
         <Route path="new-propertie" element={<NewPorpertie/>} />
       </Routes>
       <Routes>
