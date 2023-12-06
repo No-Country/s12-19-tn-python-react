8a192b38b219dfca9e99f41a8dca6166a2526f65
OG0808 <ogarns@proton.me>
2023-12-06 11:56:25 -0500

    cambios

--- Frontend/src/components/Form.jsx
+++ Frontend/src/components/Form.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
-import axiosClient from '../config/axiosClient';
 import Alert from "./Alert";
 
 const Form = () => {
+
+
+  
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
