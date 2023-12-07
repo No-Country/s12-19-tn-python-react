3da3eb93786774b47782a45fc52dd919e39d7dd4
Facundo Otero <facu_otero@hotmail.com>
2023-12-06 21:10:30 -0300

    modelo comentario

--- Backend/comentarios/views.py
+++ Backend/comentarios/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from comentarios.models import Comentario
 
 # Create your views here.
+def Comentarios(request):
+    listaComentarios = Comentario.objects.all()
+    context = {"comentarios":listaComentarios}
+    return render (request,context)
