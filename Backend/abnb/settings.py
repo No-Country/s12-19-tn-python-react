0882a71313441112a433ee2f3228ec2540d64f9e
Carlos Silva <carlossilva@ufam.edu.br>
2023-12-09 18:17:38 -0400

    refactor: agregar las apps y configuracion lo directorio media

--- Backend/abnb/settings.py
+++ Backend/abnb/settings.py
@@ -31,12 +31,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'usuario',
-    'propriedad',
+    'propiedad',
     'reserva',
     'comentarios',
     'drf_yasg',
     'rest_framework_simplejwt',
     'servicios',
+    'galeria',
 
 ]
 
@@ -149,3 +150,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
