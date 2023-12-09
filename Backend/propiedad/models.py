224d511a1455751100a62915ffd31a434c8dfc4c
Carlos Silva <carlossilva@ufam.edu.br>
2023-12-09 18:06:24 -0400

    refator: modificaciones en atributos del modelo y la estructura basica de la app propiedad

---
+++ Backend/propiedad/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+# Create your models here.
+class Propiedad (models.Model):
+    nombre = models.CharField(max_length=255,null=True)
+    descricion = models.TextField(null=True,blank=True)
+    extra_info  = models.TextField(null=True,blank=True)
+    ubicacion = models.CharField(max_length=255,null=True)
+    numero_banos = models.IntegerField(default=1)
+    numero_habitaciones = models.IntegerField(default=1)
+    invitados_max = models.IntegerField(default=1)
+    capacidad = models.IntegerField(default=0,null=True)
+    tarifa = models.DecimalField(max_digits=10, decimal_places=2,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
