85e2a341c5dde4ce81c70eded021d50fc68927a4
Carlos Silva <carlossilva@ufam.edu.br>
2023-12-10 14:32:20 -0400

    fix: conflicto en archivo resolvido

--- Backend/usuario/models.py
+++ Backend/usuario/models.py
@@ -1,11 +1,6 @@
 from django.db import models
-"""
-<<<<<<< HEAD
-from reserva.models import Reserva
-from propiedad.models import Propiedad
-=======
+
 from django.contrib.auth.models import User
->>>>>>> c1a2064a6f2aed9401ebc9a2f7e378d136624fff
 
 # Create your models here.
 class Perfil(models.Model):
@@ -14,24 +9,5 @@ class Perfil(models.Model):
     telefono = models.CharField(max_length=20)
     document_id = models.CharField(max_length=15,unique=True)
 
-
     def __str__(self):
-         return self.nombre
-
-class Anfitrion(models.Model):
-    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
-#    propiedades_administradas = models.ManyToManyField('Propiedad',blank=True)
-    avaliacao_anfitriao = models.FloatField(default=0.0)
-
-    def __str__(self):
-        return f"Anfitrion: {self.usuario.nombre}" 
-
-class Huesped (models.Model):
-    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
-#   reservas = models.ManyToManyField('Reserva', blank=True)
-    avaliacao = models.FloatField(default=0.0)
-
-=======
         return self.user.username
->>>>>>> c1a2064a6f2aed9401ebc9a2f7e378d136624fff
-"""
