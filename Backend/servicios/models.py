33fe6e6ae4b6f87223a5d9d206d47eb06c112090
Carlos Silva <carlossilva@ufam.edu.br>
2023-12-10 21:27:12 -0400

    corregir el nombre de atributo del modelo y creacion de serializer

--- Backend/servicios/models.py
+++ Backend/servicios/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from propiedad.models import Propiedad
 # Create your models here.
 class Servicio(models.Model):
-    nome = models.CharField()
-    descricion = models.TextField()
+    nombre = models.CharField()
+    descripcion = models.TextField()
     propiedad = models.ForeignKey(Propiedad, on_delete=models.CASCADE)
