72eb4e497ca640700f646b1b0f1ab807736ae404
Carlos Silva <carlossilva@ufam.edu.br>
2023-12-11 18:36:30 -0400

    refactor: agregar las urls de apps

--- Backend/abnb/urls.py
+++ Backend/abnb/urls.py
@@ -43,6 +43,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('usuario.urls')),
+    path('api/', include('reserva.urls')),
+    path('api/', include('propiedad.urls')),
+    path('api/', include('servicios.urls')),
+    path('api/', include('comentarios.urls')),
+    path('api/', include('galeria.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
