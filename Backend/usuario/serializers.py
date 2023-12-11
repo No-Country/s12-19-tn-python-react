5ce9438f4ae27bfc02a391ae2bc55083dbcbfe92
Carlos Silva <carlossilva@ufam.edu.br>
2023-12-11 18:34:43 -0400

    fix: corregir atributo de Class Meta

--- Backend/usuario/serializers.py
+++ Backend/usuario/serializers.py
@@ -5,7 +5,7 @@ from .models import Perfil
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        user = User
+        model = User
         fields = ['username','email','first_name','last_name']
 
 class PerfilSerializer(serializers.ModelSerializer):
