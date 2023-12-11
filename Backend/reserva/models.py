cf531b703dbd672e192532b0da3700736a2e9d6a
Carlos Silva <carlossilva@ufam.edu.br>
2023-12-10 21:26:47 -0400

    corregir el nombre de atributo del modelo y creacion de serializer

--- Backend/reserva/models.py
+++ Backend/reserva/models.py
@@ -7,5 +7,5 @@ class Reserva(models.Model):
     fecha_inicio = models.DateField(null=True)
     fecha_fin = models.DateField(null=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
-    propriedad = models.ForeignKey(Propiedad, on_delete=models.CASCADE,null=True)
+    propiedad = models.ForeignKey(Propiedad, on_delete=models.CASCADE,null=True)
     costo_total = models.DecimalField(max_digits=10, decimal_places=2,default=0.0)
