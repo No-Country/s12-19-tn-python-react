756be32528a7fec4f73f58f193a3f2cf196fe3a1
OG0808 <ogarns@proton.me>
2023-12-12 09:36:11 -0500

    git pull formularios

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 
         <Route path="new-propertie" element={<NewPorpertie/>} />
         <Route path="register" element={<Form/>} />
-      </Routes>
+   
       
     </>
   );
