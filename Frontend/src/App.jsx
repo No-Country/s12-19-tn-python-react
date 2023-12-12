cde763aa482daa0dbe94d279cc563404e0e14e26
Alien3k <estebanquereilhac@gmail.com>
2023-12-11 22:48:44 -0300

    implementacion de AntDesing, creacion de recuperacion de contraseña y  confirmación de cuenta

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,31 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Properties from "./components/Properties";
-
 import Inicio from "./pages/Inicio";
 import Navbar from "./components/Navbar";
 import NewPorpertie from "./Pages/NewPropertie";
-
+import Form from "./components/Form";
+import Login from "./components/Login";
+import ForgotPassword from './components/ForgotPassword';
+import NewPassword from "./components/NewPassword";
+import ConfirmAccount from "./components/ConfirmAccount";
 
 
 function App() {
   return (
     <>
-       <Nabvar/>
-       <Routes>
-       <Route  path="/" element={<Home/>} />
-       <Route  path="/properties" element={<Properties/>} />
-       </Routes>
-       
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/new-propertie" element={<NewPorpertie />} />
+        <Route path="/register" element={<Form />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path='/forgot-password/:token' element={<NewPassword/>}/>
+        <Route path='/confirm/:token' element={<ConfirmAccount/>}/>
+      </Routes>
+
     </>
   );
 }
