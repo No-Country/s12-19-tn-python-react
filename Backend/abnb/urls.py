bdae7262520d5c9cf96e20ca55c93eca9aa0e8f9
Carlos Silva <carlossilva@ufam.edu.br>
2023-12-13 17:52:25 -0400

    configuracion de url media

--- Backend/abnb/urls.py
+++ Backend/abnb/urls.py
@@ -21,6 +21,9 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
+from django.conf.urls.static import static
+from abnb import settings
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -55,4 +58,4 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
